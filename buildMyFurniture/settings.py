8cd85e32a6d4602beeeac179afde08d78ae32a61
Prashant Pokhrel <pokhrelprashant9@gmail.com>
2026-02-19 17:26:28 +0545

    fix: using dotenv

--- buildMyFurniture/settings.py
+++ buildMyFurniture/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from datetime import timedelta
+from dotenv import load_dotenv
 from pathlib import Path
 import os
 import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
