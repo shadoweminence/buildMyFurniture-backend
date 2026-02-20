8add94e995a11deac6569ce885e517eaa1cdf208
Prashant Pokhrel <pokhrelprashant9@gmail.com>
2026-02-20 15:09:31 +0545

    fix: made categories only available with authorization

--- categories/views.py
+++ categories/views.py
@@ -2,10 +2,12 @@ from categories.serializer import CategorySerializer
 from rest_framework import viewsets
 from .models import Category
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 class CategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     parser_classes = (MultiPartParser, FormParser)  # for image upload
